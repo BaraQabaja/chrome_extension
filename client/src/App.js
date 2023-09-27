dffde63a5572db84ac2c415aeb0c6f7bda0055b5
BaraQabaja <bara.qabaja@gmail.com>
2023-09-27 12:13:25 +0300

    update client lib config

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <h1>chrome extension</h1>
-      <h1>chrome extension</h1><h1>chrome extension</h1><h1>chrome extension</h1>
+      <h1>chrome extension</h1><h1>chrome extension</h1><h1>chrome extension</h1><h1>chrome extension</h1>
     </div>
   );
 }
